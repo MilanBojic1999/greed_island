2508e5a52175deb5f31343f5593008e83c9ebc09
Milan <mbojic12@gmail.com>
2023-11-17 22:15:47 +0100

    * getting loot and loot value to api

--- src/main/java/raf/deeplearning/greed_island/controller/PlayerController.java
+++ src/main/java/raf/deeplearning/greed_island/controller/PlayerController.java
@@ -1,14 +1,16 @@
 package raf.deeplearning.greed_island.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import raf.deeplearning.greed_island.Paths;
 import raf.deeplearning.greed_island.model.GameMap;
 import raf.deeplearning.greed_island.model.characters.PlayerActions;
+import raf.deeplearning.greed_island.model.loot.ILoot;
 import raf.deeplearning.greed_island.services.WorldService;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -22,29 +24,52 @@ public class PlayerController {
     }
 
     @PutMapping("/up")
-    public void upAction(){
+    public void upAction() {
         System.out.println("UP action");
-        System.out.println(GameMap.getInstance().getThePlayer()+"  WWWW  "+GameMap.getInstance());
+        System.out.println(GameMap.getInstance().getThePlayer() + "  WWWW  " + GameMap.getInstance());
         GameMap.getInstance().getThePlayer().getBufferedActions().add(PlayerActions.UP);
     }
 
     @PutMapping("/down")
-    public void downAction(){
+    public void downAction() {
         GameMap.getInstance().getThePlayer().getBufferedActions().add(PlayerActions.DOWN);
     }
 
     @PutMapping("/left")
-    public void leftAction(){
+    public void leftAction() {
         GameMap.getInstance().getThePlayer().getBufferedActions().add(PlayerActions.LEFT);
     }
 
     @PutMapping("/right")
-    public void rightAction(){
+    public void rightAction() {
         GameMap.getInstance().getThePlayer().getBufferedActions().add(PlayerActions.RIGHT);
     }
 
     @PutMapping("/wait")
-    public void waitAction(){
+    public void waitAction() {
         GameMap.getInstance().getThePlayer().getBufferedActions().add(PlayerActions.WAIT);
+    }
+
+    @GetMapping("/inventory")
+    public Map<String, Integer> getLoot() {
+        return GameMap.getInstance().getThePlayer().getBagOfLoot();
+    }
+
+    @GetMapping("/inventory/value")
+    public Integer getLootValue() {
+        Map<String,Integer> map =  GameMap.getInstance().getThePlayer().getBagOfLoot();
+        int full_value = 0;
+        for(String lootName:map.keySet()) {
+            try {
+                Class<? extends ILoot> loot = Class.forName("raf.deeplearning.greed_island.model.loot."+lootName).asSubclass(ILoot.class);
+                full_value += loot.getConstructor().newInstance().getValueInGold() * map.get(lootName);
+            } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
+                     InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        return full_value;
     }
 }
