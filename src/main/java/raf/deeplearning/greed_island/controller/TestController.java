a3166fc9f9b7a9018254774675468e991950cef8
mboji <mbojic12@gmail.com>
2023-11-27 17:23:49 +0100

    * Return matrix of mapp

--- src/main/java/raf/deeplearning/greed_island/controller/TestController.java
+++ src/main/java/raf/deeplearning/greed_island/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raf.deeplearning.greed_island.model.GameMap;
+import raf.deeplearning.greed_island.model.spaces.ASpace;
 import raf.deeplearning.greed_island.services.WorldService;
 
 import java.util.Arrays;
@@ -29,4 +30,17 @@ public class TestController {
         return Arrays.toString(GameMap.getInstance().getSpaces());
     }
 
+    @GetMapping("/full/matrix")
+    public String[][] getFullHelloMatrix() {
+        ASpace[][] spaces = GameMap.getInstance().getSpaces();
+        String[][] matrix = new String[spaces.length][spaces[0].length];
+
+        for(int i = 0; i < spaces.length; i++) {
+            for(int j = 0; j < spaces[i].length; j++) {
+                matrix[i][j] = String.valueOf(spaces[i][j].getSpaceSymbol());
+            }
+        }
+
+        return matrix;
+    }
 }
